be6c1c9dc5f4776fe24a135d23d35725055e0398
jpbetanza <jpbetanza99@gmail.com>
2024-01-08 10:51:56 -0300

    links do footer agora abrem novas guias

--- FrontEnd/src/components/Footer/Footer.jsx
+++ FrontEnd/src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () =>{
     return <div className="bottom-0 h-10 w-full bg-xdark text-white flex justify-between gap-2 items-center px-5 ">
         <h1 className="text-xs sm:text-base"> Criadores: 
-           <a className="text-medium" href="https://github.com/jpbetanza"> @jpbetanza </a> e 
-          <a className="text-medium" href="https://github.com/viniciussidrim"> @viniciussidrim</a>
+           <a className="text-medium" target="_blank" href="https://github.com/jpbetanza"> @jpbetanza </a> e 
+          <a className="text-medium" target="_blank" href="https://github.com/viniciussidrim"> @viniciussidrim</a>
         </h1>
         <h1 className="text-xs sm:text-base">
-        <a className="text-medium" href="https://github.com/jpbetanza"> Github </a>
+        <a className="text-medium" target="_blank" href="https://github.com/jpbetanza"> Github </a>
              do projeto
         </h1>
     </div>
